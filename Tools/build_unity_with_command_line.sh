b72a312f6a5057c88216f3e606115ba31f73de35
Kevin Chang <kevin@megalive.com.tw>
2018-02-27 11:31:04 +0800

    test5

--- Tools/build_unity_with_command_line.sh
+++ Tools/build_unity_with_command_line.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 UNITY_PATH=/Applications/Unity/Unity.app/Contents/MacOS/Unity
-PROJECT_PATH=/Users/kevin/Downloads/UnityCommandLineBuild-master
+PROJECT_PATH=/Users/kevin/Desktop/JenkeinsUnityBuildTest
 BUILD_LOG_PATH=${PROJECT_PATH}/build.log
-DESTINATION_PATH=/Users/kevin/Downloads/UnityCommandLineBuild-master/Tools
+DESTINATION_PATH=/Users/kevin/Desktop/JenkeinsUnityBuildTest/Tools
 
 $UNITY_PATH -quit -batchmode -projectPath ${PROJECT_PATH} -executeMethod BuildTool.Build -logFile ${BUILD_LOG_PATH} -destinationPath ${DESTINATION_PATH}
